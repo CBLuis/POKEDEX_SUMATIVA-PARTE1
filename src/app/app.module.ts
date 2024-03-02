a3fc440c3db12d807c78f20b52b404ce65012e87
CBLuis <145160720+CBLuis@users.noreply.github.com>
2024-03-01 20:59:39 -0500

    arreglado2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -13,11 +13,24 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,HttpClientModule ,IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "pokedexcarpiogaibor",
+      "appId": "1:6059268158:web:6511bf89bc05184e11596f",
+      "storageBucket": "pokedexcarpiogaibor.appspot.com",
+      "apiKey": "AIzaSyBd3IrO2CMdYKrIF4-KoiBkA5mx9wjtKgc",
+      "authDomain": "pokedexcarpiogaibor.firebaseapp.com",
+      "messagingSenderId": "6059268158"
+    })),
     provideFirestore(() => getFirestore()),
-    provideFirebaseApp(() => initializeApp({"projectId":"pokedexcarpiogaibor","appId":"1:6059268158:web:6511bf89bc05184e11596f","storageBucket":"pokedexcarpiogaibor.appspot.com","apiKey":"AIzaSyBd3IrO2CMdYKrIF4-KoiBkA5mx9wjtKgc","authDomain":"pokedexcarpiogaibor.firebaseapp.com","messagingSenderId":"6059268158"}))],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
-  
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
